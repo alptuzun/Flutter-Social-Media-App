acdf8034f7a6b7e6c86763009ea9604badd47f31
alptuzun <alptuzun@sabanciuniv.edu>
2022-05-20 00:55:52 +0300

    created chat screen

---
+++ lib/ui/chat_message.dart
@@ -0,0 +1,49 @@
+class Chat {
+  String message;
+  String type;
+
+  Chat({required this.message, required this.type});
+}
+
+List<Chat> messageList = [
+  Chat(
+    message: "Hello kid how are you?",
+    type: "sender",
+  ),
+  Chat(
+    message: "Not much MJ, how are you?",
+    type: "receiver",
+  ),
+  Chat(
+    message: "I'm fine, hey you coming to the game.",
+    type: "sender",
+  ),
+  Chat(
+    message: "I got court-side seats reserved for you.",
+    type: "sender",
+  ),
+  Chat(
+    message: "Interested?",
+    type: "sender",
+  ),
+  Chat(
+    message: "Where, when?",
+    type: "receiver",
+  ),
+  Chat(
+    message: "Staples Center, next week.",
+    type: "sender",
+  ),
+  Chat(
+    message: "YEAH, I would love to.",
+    type: "receiver",
+  ),
+  Chat(
+    message: "See you there then young blood",
+    type: "sender",
+  ),
+  Chat(
+    message: "BTW You owe me 10 cigars, don't you forget it",
+    type: "sender",
+  ),
+];
