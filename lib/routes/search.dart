a08545f8f24fe735263ff7c9f33dcf5dc1981205
YasinAlbayrak <yasinalbayrak@sabanciuniv.edu>
2022-06-12 16:03:03 +0300

    added following functionality
    added user_services take a look for new functions

--- lib/routes/search.dart
+++ lib/routes/search.dart
@@ -1,11 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cs310_group_28/services/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:cs310_group_28/visuals/fade_animation.dart';
 import 'package:cs310_group_28/visuals/colors.dart';
 import 'package:cs310_group_28/services/user_service.dart';
-import 'package:cs310_group_28/models/user.dart';
 
+import '../models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
   static const String routeName = "/explore_SearchScreen";
@@ -16,27 +21,65 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   List<MyUser> _foundUsers = [];
-  List<MyUser> listOfUsers = [];
+  List<MyUser> list_of_users = [];
+  //List followers = [];
+  List followings = [];
+  bool isFollowing = false;
+  final TextEditingController searchcontrol = TextEditingController();
 
   @override
-  void initState() {
-    getList();
-    super.initState();
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    TextEditingController().dispose();
   }
 
-  Future getList() async {
+  @override
+  void initState()  {
+    super.initState();
+    prt();
+    getUsersFollowers();
+    print(followings);
+    //handleFollowUser();
+  }
+  isfollows(dynamic user_id) async {
+
+    var result = await UserService.isfollowing( uid: FirebaseAuth.instance.currentUser!.uid, followinguserid: user_id);
+    setState(() {
+      isFollowing = result;
+    });
+
+  }
+  Future getUsersFollowers() async {
+    var user =
+    await UserService.getFollowings(FirebaseAuth.instance.currentUser!.uid);
+    setState(() {
+      followings = user;
+    });
+    print('Followings are:');
+    print(followings[1]=='X9nyMA5q6XePW6JH3320xLVZq6F3' ); //== " X9nyMA5q6XePW6JH3320xLVZq6F3X9nyMA5q6XePW6JH3320xLVZq6F3"
+  }
+  Future updateFollowing() async{
+    List list = await UserService.getFollowings(FirebaseAuth.instance.currentUser!.uid);
+    setState(() {
+      followings = list;
+    });
+
+  }
+
+  Future prt() async {
     List<MyUser> users = await UserService.getAllUsers();
     setState(() {
       _foundUsers = users;
-      listOfUsers = users;
+      list_of_users = users;
     });
-  }
 
+  }
   onSearch(String search) {
     setState(() {
-      _foundUsers = listOfUsers
-          .where((element) => element.fullName.toLowerCase().contains(search))
-          .toList();
+      _foundUsers =
+          list_of_users.where((element) => element.fullName.toLowerCase().contains(search))
+              .toList();
     });
   }
 
@@ -45,6 +88,7 @@ class _SearchScreenState extends State<SearchScreen> {
       _foundUsers.removeAt(whichUser);
     });
   }
+  bool usershowing = false;
 
   @override
   Widget build(BuildContext context) {
@@ -68,6 +112,7 @@ class _SearchScreenState extends State<SearchScreen> {
           title: SizedBox(
             height: 40,
             child: TextField(
+              controller: searchcontrol,
               onChanged: (val) => onSearch(val),
               decoration: InputDecoration(
                   filled: true,
@@ -82,66 +127,127 @@ class _SearchScreenState extends State<SearchScreen> {
                       borderSide: BorderSide.none),
                   hintText: "Search",
                   hintStyle: const TextStyle(
-                      fontSize: 14, color: Color.fromARGB(255, 131, 131, 131))),
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 131, 131, 131))),
             ),
           ),
         ),
-        body: Container(
-            color: const Color.fromARGB(255, 245, 245, 245),
-            child: _foundUsers.isEmpty
-                ? const Center(
-                    child: Text(
-                      "No user has been found",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  )
-                : ListView.builder(
-                    itemCount: _foundUsers.length,
-                    itemBuilder: (ctx, index) {
-                      return FadeAnimation(
-                          delay: 0.05 * index,
-                          child: Slidable(
-                            child: userPart(aUser: _foundUsers[index]),
-                            actionPane: const SlidableStrechActionPane(),
-                            actionExtentRatio: 0.25,
-                            actions: const [
-                              IconSlideAction(
-                                caption: "Archive",
-                                color: Color.fromARGB(255, 236, 236, 236),
-                                iconWidget: Icon(
-                                  Icons.archive,
-                                  color: Colors.black,
-                                ),
+        body: FutureBuilder(
+          future: FirebaseFirestore.instance.collection('Users').where('username',isGreaterThanOrEqualTo:searchcontrol.text ).get(),
+          builder: (context,snapshot){
+            if(!snapshot.hasData){
+              return const Center(child: CircularProgressIndicator());
+            }
+            return Container(
+
+              color: const Color.fromARGB(255, 245, 245, 245),
+
+              child: ListView.builder(
+                itemCount: (snapshot.data! as dynamic).docs.length ,
+                itemBuilder: (ctx, index) {
+
+                  return
+                    FadeAnimation(
+                        delay: 0.05 * index,
+                        child: Slidable(
+                          child: userPart(aUser: (snapshot.data! as dynamic).docs[index]),
+                          actionPane: const SlidableStrechActionPane(),
+                          actionExtentRatio: 0.25,
+                          actions: const [
+                            IconSlideAction(
+                              caption: "Archive",
+                              color: Color.fromARGB(255, 236, 236, 236),
+                              iconWidget: Icon(
+                                Icons.archive,
+                                color: Colors.black,
                               ),
-                              IconSlideAction(
-                                caption: 'Share',
-                                color: Color.fromARGB(255, 236, 236, 236),
-                                iconWidget: Icon(
-                                  Icons.share,
-                                  color: Colors.black,
-                                ),
-                                onTap: null,
+                            ),
+                            IconSlideAction(
+                              caption: 'Share',
+                              color: Color.fromARGB(255, 236, 236, 236),
+                              iconWidget: Icon(
+                                Icons.share,
+                                color: Colors.black,
                               ),
-                            ],
-                            secondaryActions: [
-                              IconSlideAction(
-                                caption: 'Remove',
-                                color: Colors.red,
-                                iconWidget: const Icon(
-                                  Icons.delete,
-                                  color: Colors.white,
-                                ),
-                                onTap: () => userRemoveWithIndex(index),
+                              onTap: null,
+                            ),
+                          ],
+                          secondaryActions: [
+                            IconSlideAction(
+                              caption: 'Remove',
+                              color: Colors.red,
+                              iconWidget: const Icon(
+                                Icons.delete,
+                                color: Colors.white,
                               ),
-                            ],
-                          ));
-                    },
-                  )),
+                              onTap: () => userRemoveWithIndex(index),
+                            ),
+                          ],
+                        ));
+                },
+              ),
+            );
+          },
+          /*child: Container(
+
+              color: const Color.fromARGB(255, 245, 245, 245),
+
+              child: _foundUsers.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "No user has been found",
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: _foundUsers.length,
+                      itemBuilder: (ctx, index) {
+                        return FadeAnimation(
+                            delay: 0.05 * index,
+                            child: Slidable(
+                              child: userPart(aUser: _foundUsers[index]),
+                              actionPane: const SlidableStrechActionPane(),
+                              actionExtentRatio: 0.25,
+                              actions: const [
+                                IconSlideAction(
+                                  caption: "Archive",
+                                  color: Color.fromARGB(255, 236, 236, 236),
+                                  iconWidget: Icon(
+                                    Icons.archive,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                IconSlideAction(
+                                  caption: 'Share',
+                                  color: Color.fromARGB(255, 236, 236, 236),
+                                  iconWidget: Icon(
+                                    Icons.share,
+                                    color: Colors.black,
+                                  ),
+                                  onTap: null,
+                                ),
+                              ],
+                              secondaryActions: [
+                                IconSlideAction(
+                                  caption: 'Remove',
+                                  color: Colors.red,
+                                  iconWidget: const Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                  ),
+                                  onTap: () => userRemoveWithIndex(index),
+                                ),
+                              ],
+                            ));
+                      },
+                    )
+          ),*/
+        ),
       ),
     );
   }
 
-  userPart({required MyUser aUser}) {
+  userPart({required dynamic aUser}) {
     return Container(
         margin: const EdgeInsets.symmetric(horizontal: 20),
         padding: const EdgeInsets.only(top: 10, bottom: 10),
@@ -157,21 +263,21 @@ class _SearchScreenState extends State<SearchScreen> {
                     child: CircleAvatar(
                       radius: 60,
                       backgroundImage:
-                          CachedNetworkImageProvider(aUser.profilePicture),
+                      CachedNetworkImageProvider(aUser['profilePicture']),
                     ),
                   )),
               const SizedBox(width: 10),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(aUser.fullName,
+                  Text(aUser['fullName'],
                       style: const TextStyle(
                           color: Color.fromARGB(255, 15, 15, 15),
                           fontWeight: FontWeight.w700)),
                   const SizedBox(
                     height: 5,
                   ),
-                  Text(aUser.username,
+                  Text(aUser['username'],
                       style: const TextStyle(
                           color: Color.fromARGB(255, 90, 90, 90))),
                 ],
@@ -179,8 +285,8 @@ class _SearchScreenState extends State<SearchScreen> {
             ]),
             GestureDetector(
               onTap: () {
-                setState(() {
-                  aUser.private = !aUser.private; //do here
+                setState(()  {
+                  user_try_following(aUser);
                 });
               },
               child: AnimatedContainer(
@@ -188,24 +294,30 @@ class _SearchScreenState extends State<SearchScreen> {
                 width: 110,
                 duration: const Duration(milliseconds: 250),
                 decoration: BoxDecoration(
-                    color:
-                        aUser.private ? Colors.blue[700] : Colors.transparent,
+                    color: (followings.contains(aUser['userID'],) )
+                        ? Colors.blue[700]
+                        : Colors.transparent,
                     borderRadius: BorderRadius.circular(7),
                     border: Border.all(
-                        color: aUser.private
+                        color: (followings.contains(aUser['userID']))
                             ? Colors.transparent
                             : Colors.grey.shade700, // if statement
                         width: 1)),
-                child: Center(
-                  child: Text(
-                    aUser.private ? "Unfollow" : "Follow",
+                child:  Center(
+                  child:  Text(
+                    followings.contains(aUser['userID']) ? "Unfollow" : "Follow",
                     style: TextStyle(
-                        color: aUser.private ? Colors.white : Colors.blue),
+                        color: followings.contains(aUser['userID']) ? Colors.white : Colors.blue),
                   ),
-                ),
+                ) ,
               ),
             )
           ],
         ));
+  }
+
+  void user_try_following(dynamic aUser) async{
+    await UserService.followUser(FirebaseAuth.instance.currentUser!.uid,aUser['userID'],);
+    await updateFollowing();
   }
 }
